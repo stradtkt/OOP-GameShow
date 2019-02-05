298bffc78a09e36d6f7c3b2a2081a688d6fc7cb3
Kevin Stradtman <stradtkt22@gmail.com>
2019-02-04 21:50:32 -0500

    Added some of class Phrase

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,39 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase;
+        this.letters = [...this.phrase];
+        this.squares = [];
+    }
+
+    addToDisplay(target) {
+        this.letters.forEach((el) => {
+            const li = document.createElement('li');
+            li.textContent = el;
+            li.style.zIndex = -1;
+            if(el === ' ') {
+                li.className = 'hide space';
+            } else {
+                li.className = `hide letter ${el}`;
+            }
+            this.squares.push(li);
+            target.appendChild(li)
+        });
+
+        this.squares.forEach((box) => {
+           if(box.classList.contains('letter')) {
+               box.classList.remove('hide');
+               box.className += 'animated zoomIn';
+           }
+        });
+    }
+    
+    checkLetter(target) {
+        return this.letters.indexOf(target) === -1 ? true : false;
+    }
+
+    
+}
