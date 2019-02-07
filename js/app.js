773afbcd3a901f16722fb8b2ee8eddcb9f31d1be
Kevin Stradtman <stradtkt22@gmail.com>
2019-02-07 14:07:03 -0500

    Added app.js

--- js/app.js
+++ js/app.js
@@ -16,3 +16,65 @@
      resetDisplay();
      return game;
  });
+
+
+ function resetDisplay() {
+     overlay.classList.value = 'start animated slideOutUp';
+ }
+
+ function markButton(target, key) {
+     target.setAttribute('disabled', 'true');
+     target.classList.value += ' chosen';
+     game.handleInteraction(key);
+ }
+
+ keys.forEach((key) => {
+    key.addEventListener('click', (event) => {
+      if (game.started) {
+        eventHandler(event.type, event.target);
+      };
+    }, false);
+  });
+  document.addEventListener('keypress', (event) => {
+    if (game.started) {
+      eventHandler(event.type, event);
+    };
+  }, false);
+  
+  // Event handler function for the click and keypress event
+  function eventHandler(eventType, targetOrObject) {
+    switch (eventType) {
+      case 'click': {
+        if (targetOrObject.classList.contains('key')) {
+          const letter = targetOrObject.textContent;
+          markButton(targetOrObject, letter);
+        }
+        break;
+      }
+      case 'keypress': {
+        const keycode = targetOrObject.key;
+        const keyboardKey = [...keys];
+        const keyToDisable = keyboardKey.filter((key) => key.textContent === keycode);
+        markButton(...keyToDisable, keycode);
+        break;
+      }
+    }
+  };
+  
+  // Function to reset the game
+  function resetGame() {
+    const squares = document.querySelectorAll('#phrase ul li');
+    const hearts = document.querySelectorAll('#scoreboard ol li');
+  
+    squares.forEach((box) => phrase.removeChild(box));
+    hearts.forEach((heart) => heart.className= 'tries');
+  
+    startBtn.textContent = '';
+  
+    keys.forEach((key) => {
+      key.removeAttribute('disabled');
+      key.classList.remove('chosen');
+    });
+  
+    game = undefined;
+  }
