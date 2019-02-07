b72fa43dcbed2e5e047ddf1570f075dea818b435
Kevin Stradtman <stradtkt22@gmail.com>
2019-02-07 11:57:51 -0500

    Added some of the main app.js file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ const overlay = document.getElementById('overlay');
+ const startBtn = document.querySelector('#overlay #btn__reset');
+ const phrase = document.querySelector('#phrase ul');
+ const keys = document.querySelectorAll('.key');
+
+ let game;
+
+ startBtn.addEventListener('click', () => {
+     resetGame();
+     game = new Game();
+     game.startGame(phrase);
+     resetDisplay();
+     return game;
+ });
